fa960d0965a6acc86845779f42f6f887773edae0
Sara Vieira <hey@iamsaravieira.com>
2018-08-29 19:38:34 +0200

    add stickers

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import BGImage from './bg-image'
+import CodeSanbox from './codeSanbox'
+import LayoutNoFooter from './layout-no-footer'
+import Layout from './layout'
+import Split from './Split'
+import Theme from './theme'
+import webpackRules from './webpackRules'
+
+export { BGImage, CodeSanbox, LayoutNoFooter, Layout, Split, webpackRules }
+
+export default Theme
